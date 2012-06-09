1528e2ea8d7c4e19e8bcf835bc217461810b3ed0
sethyx <sethyx@gmail.com>
2012-06-09 19:19:01 +0200

    AOKPStats - anonymous user statistics. credit: CM
    Patch set 4: rebased, modified regexp to match jellybean releases
    
    ro.aokp.version should be aokp_device_jb-build-x or aokp_device_jb-milestone-y from now on
    
    Signed-off-by: sethyx <sethyx@gmail.com>
    
    Change-Id: I5fbee961dd9485c894f0b0eef83bbacf3979e2b8
    
    Conflicts:
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -844,5 +844,6 @@ public class Settings extends PreferenceActivity
     public static class TextToSpeechSettingsActivity extends Settings { /* empty */ }
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
+    public static class AnonymousStatsActivity extends Settings { /* empty */ }
     public static class DreamSettingsActivity extends Settings { /* empty */ }
 }
